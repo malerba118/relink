839cd76a58556bbe8bd19b59b88bbf66ce411497
Austin Malerba <austin.malerba@gmail.com>
2022-01-09 18:55:11 -0700

    local auth redirect

--- client/api/auth.ts
+++ client/api/auth.ts
@@ -2,9 +2,17 @@ import { supabase } from "./supabase";
 import { assertResponseOk } from "./utils";
 
 export async function signIn() {
-  const response = await supabase.auth.signIn({
-    provider: "twitter",
-  });
+  const response = await supabase.auth.signIn(
+    {
+      provider: "twitter",
+    },
+    {
+      redirectTo:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000"
+          : "https://relink.page",
+    }
+  );
   assertResponseOk(response);
   return response.session;
 }
